69b2c88e6354852eef5b565f807e63cab5ddcdd7
Nhat Loi <nhatloi120402@gmail.com>
2025-09-25 11:28:42 +0700

    hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import Navbar from '../components/Navbar'
+import Hero from '../sections/Hero'
 
 export default function HomePage() {
   return (
     <>
-      <Navbar />
+      <header className='absolute top-0 left-0 w-full z-50'>
+        <Navbar />
+      </header>
+      <Hero />
     </>
   )
 }
